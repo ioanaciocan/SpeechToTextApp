86990fe66c3e43803ce89a1b1d540427f32d9262
ioanaciocan <ciocancioana@gmail.com>
2021-07-19 15:12:54 +0300

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
+
+namespace Client
+{
+    public class Client
+    {
+        static string filename;
+        static string port;
+        static string key;
+
+        async Task DecodeFile()
+        {
+            ClientWebSocket ws = new ClientWebSocket();
+            await ws.ConnectAsync(new Uri("ws://live-transcriber.zevo-tech.com:" + port), CancellationToken.None);
+
+            byte[] api = Encoding.UTF8.GetBytes("{\"config\": {\"key\": \"" + key + "\"}}");
+            await ws.SendAsync(new ArraySegment<byte>(api, 0, api.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            FileStream fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read);
+
+            byte[] data = new byte[16000];
+
+            while (true)
+            {
+                int count = fileStream.Read(data, 0, 16000);
+                if (count == 0) break;
+
+                await ProcessData(ws, data, count);
+            }
+
+            await ProcessFinalData(ws);
+
+            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "OK", CancellationToken.None);
+        }
+
+        async Task ProcessData(ClientWebSocket ws, byte[] data, int count)
+        {
+            await ws.SendAsync(new ArraySegment<byte>(data, 0, count), WebSocketMessageType.Binary, true, CancellationToken.None);
+            await RecieveResult(ws);
+        }
+
+        async Task ProcessFinalData(ClientWebSocket ws)
+        {
+            byte[] eof = Encoding.UTF8.GetBytes("{\"eof\" : 1}");
+            await ws.SendAsync(new ArraySegment<byte>(eof), WebSocketMessageType.Text, true, CancellationToken.None);
+            await RecieveResult(ws);
+        }
+
+        async Task RecieveResult(ClientWebSocket ws)
+        {
+            byte[] result = new byte[4096];
+            Task<WebSocketReceiveResult> receiveTask = ws.ReceiveAsync(new ArraySegment<byte>(result), CancellationToken.None);
+            await receiveTask;
+            var receivedString = Encoding.UTF8.GetString(result, 0, receiveTask.Result.Count);
+            Console.WriteLine("Result {0}", receivedString);
+        }
+
+        public static void Main(string file)
+        {
+
+            filename = file;
+            port = "12320";
+            key = "e4b70d22ca4b47369fbbc46b2afa3c33";
+
+            Task.Run(async () =>
+            {
+                await new Client().DecodeFile();
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.System.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Client;
+
+namespace SpeechToTextApp
+{
+    public sealed partial class MainPage : Page
+    {
+
+        StorageFile file;
+
+        MediaCapture mediaCapture;
+
+        LowLagMediaRecording _mediaRecording;
+
+        DisplayRequest displayRequest = new DisplayRequest();
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            btnRec.IsEnabled = true;
+            btnStop.IsEnabled = false;
+
+            var taskInitializeMediaCaptureAsync = Task.Run(async () => await InitializeMediaCaptureAsync());
+
+        }
+
+        async Task InitializeMediaCaptureAsync()
+        {
+            mediaCapture = new MediaCapture();
+            await mediaCapture.InitializeAsync();
+            //mediaCapture.Failed += MediaCapture_Failed;
+        }
+
+        async Task startRecording()
+        {
+            
+            var myVideos = await Windows.Storage.StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Videos);
+            file = await myVideos.SaveFolder.CreateFileAsync("audio.wav", CreationCollisionOption.GenerateUniqueName);
+            _mediaRecording = await mediaCapture.PrepareLowLagRecordToStorageFileAsync(
+                    MediaEncodingProfile.CreateWav(AudioEncodingQuality.High), file);
+            await _mediaRecording.StartAsync();
+        }
+
+        async Task stopRecording()
+        {
+            await _mediaRecording.StopAsync();
+        }
+
+        void btnRec_Click(object sender, RoutedEventArgs e)
+        {
+            btnStop.IsEnabled = true;
+            btnRec.IsEnabled = false;
+
+            Task taskStartRecording = Task.Run(async () => await startRecording());
+        }
+
+        void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            btnStop.IsEnabled = false;
+            btnRec.IsEnabled = true;
+
+            Task taskStopRecording = Task.Run(async () => await stopRecording());
+
+            Client.Client.Main(file.n);
+        }
+    }
+}
